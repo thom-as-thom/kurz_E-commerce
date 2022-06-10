9b9f2d15b202ea4df0dbc8e4e453a3d3b8ec80ac
Thomas Kurz <thomaskurz.15@gmail.com>
2022-06-10 15:56:27 -0300

    eliminated password field

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,7 +1,9 @@
-import {React} from "react";
+import { React } from "react";
 import './form.css'
 
 function Form({ handleSubmit, handleChange, GenerateBuyOrder, user }) {
+
+   
 
     return (
         <form onSubmit={handleSubmit}>
@@ -11,7 +13,7 @@ function Form({ handleSubmit, handleChange, GenerateBuyOrder, user }) {
                 <input type="text" name="email" onChange={handleChange}></input>
                 <label htmlFor="phoneNumber"> Phone Number </label>
                 <input type="text" name="phoneNumber" onChange={handleChange}></input>
-            <button className={!user.fullName||!user.email||!user.phoneNumber ? 'disabled': 'enabled'} disabled={!user.fullName||!user.email||!user.phoneNumber} onClick={GenerateBuyOrder}>FINALIZAR COMPRA</button>
+            <button id="submit" className={!user.fullName||!user.email||!user.phoneNumber ? 'disabled': 'enabled'} disabled={!user.fullName||!user.email||!user.phoneNumber} onClick={GenerateBuyOrder}>FINALIZAR COMPRA</button>
             </form>
     )
 }
