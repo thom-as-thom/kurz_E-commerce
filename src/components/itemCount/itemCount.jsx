40e6dc38a13033d7d70c4e947697a78f68cd5757
Thomas Kurz <thomaskurz.15@gmail.com>
2022-06-14 17:29:59 -0300

    functionality adapted to work in the cart items aswell

--- src/components/itemCount/itemCount.jsx
+++ src/components/itemCount/itemCount.jsx
@@ -3,19 +3,25 @@ import { useState } from 'react'
 import './itemCount.css'
 
 
-function ItemCount({stock, initial, onAdd, id}) {
+function ItemCount({stock, initial, onAdd, id, onChange}) {
 
-    const  [number, setNumber] = useState(1)
+    const  [number, setNumber] = useState(Number(initial))
     function increase(){
         if(number<stock)
         {
-            setNumber (Number(number)+1)
+            setNumber(Number(number) + 1)
+            if (onChange) {
+                onChange(+1)
+            }
         }
         
     }
     function decrease (){
-        if (number > initial){
-            setNumber (number-1)
+        if (number > 1) {
+            setNumber(number - 1)
+            if (onChange) {
+                onChange(-1)
+            }
         }
     }
     function addToCart (){
@@ -32,7 +38,12 @@ function ItemCount({stock, initial, onAdd, id}) {
                     <input id='inputNum'  type="text"  max={stock} min='1' readOnly value={number}/>
                     <span className='buttonSpan' onClick={increase}> + </span>
                 </section>
-                    <button  onClick={addToCart}>agregar al carrito</button>
+                {
+                    onAdd ?
+                        <button onClick={addToCart}>agregar al carrito</button>
+                        :
+                        <></>
+                }
             </div>
         )
 }
